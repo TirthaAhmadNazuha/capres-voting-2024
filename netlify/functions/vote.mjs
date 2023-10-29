8a393d752c7d5e51db372bce42c4303b9b2fa3cf
TirthaAhmadNazuha <tirthaahmadnazuha.udah@gmail.com>
2023-10-28 20:23:54 -1000

    now data saved on public/data-vote.json

--- netlify/functions/vote.mjs
+++ netlify/functions/vote.mjs
@@ -1,33 +1,28 @@
-export const voters = {
-  'no-1': {
-    nama: 'Ganjar Pranowo',
-    votes: new Set()
-  },
-  'no-2': {
-    nama: 'Prabowo Subianto',
-    votes: new Set()
-  },
-  'no-3': {
-    nama: 'Anis Baswedan',
-    votes: new Set()
-  },
+
+import { writeFile, readFile } from 'fs/promises';
+
+const saveToFlie = async (voters) => {
+  let dataVoters = JSON.stringify(voters);
+  await writeFile('./public/data-vote.json', dataVoters, 'utf-8');
 };
 
 export const handler = async (req, context) => {
   const { capresId, voterId, type } = JSON.parse(req.body);
+  /**@type {Object<string, { votes: string[] }>} */
+  const voters = JSON.parse(await readFile('./public/data-vote.json', 'utf-8'));
   if (type == 'get') return {
     statusCode: 200,
     body: JSON.stringify({
-      'no-1': voters['no-1'].votes.size,
-      'no-2': voters['no-2'].votes.size,
-      'no-3': voters['no-3'].votes.size,
+      'no-1': voters['no-1'].votes.length,
+      'no-2': voters['no-2'].votes.length,
+      'no-3': voters['no-3'].votes.length,
     })
   };
 
   if (type == 'find') {
     let result = null;
     Object.keys(voters).forEach((key) => {
-      if (result == null && voters[key].votes.has(voterId)) {
+      if (result == null && voters[key].votes.find((id) => id == voterId)) {
         result = key;
       }
     });
@@ -38,10 +33,11 @@ export const handler = async (req, context) => {
   }
 
   if (type == 'post') {
-    voters[capresId].votes.add(voterId);
+    voters[capresId].votes.push(voterId);
   } else if (type == 'delete') {
-    voters[capresId].votes.delete(voterId);
+    voters[capresId].votes.splice(voters[capresId].votes.findIndex((id) => id == voterId));
   }
+  saveToFlie(voters);
 
   return {
     statusCode: 200,
